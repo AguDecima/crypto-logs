2fb98d82112c25633dae2f1d261635d8cd400d57
AguDecima <agudecima@gmail.com>
2020-10-31 18:12:44 -0300

    feat: logic is added to create hashes that start with two zeros and are saved in csv

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,5 +1,8 @@
 const {schemaValidation} = require('../utils/validationShema.utils');
 const {messsageSchema} = require('./schema/message.schema');
+const {getLogData, generateRandomHash, 
+    setLogData, computeHashProofWork,
+    generateHash} = require('../services/hash.service');
 
 const receiveMessage = async (req, res) => {   
 
@@ -9,14 +12,29 @@ const receiveMessage = async (req, res) => {
         schemaValidation('receiveMessage',payload, messsageSchema);
     } catch (error) {
         // add log
-        res.status(400).json({
+        return res.status(400).json({
             message: 'invalid body',
             code: 'bad_request'
         });
         return;
     }
 
-    res.status(200).json(payload);
+    let {message} = payload;
+    const logs = await getLogData();
+    let prevHash;
+    if(!logs.length) prevHash = generateRandomHash();
+    else {
+        let lastLog = logs[logs.length - 1];
+        prevHash = generateHash(`${lastLog[0]},${lastLog[1]},${lastLog[2]}`);
+    }
+
+    const nonce = computeHashProofWork(prevHash,message);
+    logs.push(`${prevHash},${message},${nonce}`.split(','));
+    await setLogData(logs);
+    return res.status(200).json({
+        messag: 'the message was sent'
+    });
+    
 }
 
 module.exports = {
---
+++ services/hash.service.js
@@ -0,0 +1,63 @@
+const crypto = require('crypto');
+const fs = require('fs').promises;
+const parse = require('csv-parse/lib/sync');
+const stringify = require('csv-stringify');
+
+const generateRandomHash = () => {
+    let currentDate = (new Date()).valueOf().toString(); 
+    const regex = new RegExp('^00.*');
+    let nonce = 0;
+    while(true) {
+        let hash = crypto.createHash('sha256')
+                    .update(`${currentDate},${nonce}`)
+                    .digest('hex')
+                    .toString();
+        if(regex.exec(hash)) return hash;
+        else nonce += 1;
+    }
+}
+
+const generateHash = (data) => {
+    return crypto.createHash('sha256')
+                    .update(data)
+                    .digest('hex')
+                    .toString();
+}
+
+const computeHashProofWork = (prevHash,message) => {
+    let nonce = 0;
+    const regex = new RegExp('^00.*');
+    while (true) {
+        let hash = generateHash(`${prevHash},${message},${nonce}`);
+        if(regex.exec(hash)) return nonce;
+        else nonce += 1;
+    }
+};
+
+const getLogData = async () => {
+    try {
+        const fileContent = await fs.readFile(__dirname+'/crypto-logs.csv');
+        let records = await parse(fileContent, { columns: false });
+        return records;
+    } catch (error) {
+        // add log
+        return [];
+    }
+}
+
+const setLogData = async (newData) => {
+    stringify(newData, {
+        header: false
+      }, async function (err, output) {
+        if (err) throw err;
+        await fs.writeFile(__dirname+'/crypto-logs.csv', output);
+      });
+}
+
+module.exports = {
+    generateHash,
+    getLogData,
+    setLogData,
+    generateRandomHash,
+    computeHashProofWork
+};
